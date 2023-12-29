17868cb3a60b02a7dc7ebf3cda769159f1355f34
BazlaTariq <154462359+BazlaTariq@users.noreply.github.com>
2023-12-29 09:15:07 +0500

    Update 12-Bazla Tariq-460547-AE-01-Projectno1.cpp

--- 12-Bazla Tariq-460547-AE-01-Projectno1.cpp
+++ 12-Bazla Tariq-460547-AE-01-Projectno1.cpp
@@ -27,63 +27,37 @@ void function2() {
         cout << p1 << " Please enter: ";
         cin >> digit;
     }
-    if (token == '0') {
+    else if (token == '0') {
         cout << p2 << " Please enter: ";
         cin >> digit;
-    }
-    if (digit == 1) {
-        row = 0;
-        column = 0;
-    }
-    else if (digit == 2) {
-        row = 0;
-        column = 1;
-    }
-    else if (digit == 3) {
-        row = 0;
-        column = 2;
-    }
-    else if (digit == 4) {
-        row = 1;
-        column = 0;
-    }
-    else if (digit == 5) {
-        row = 1;
-        column = 1;
-    }
-    else if (digit == 6) {
-        row = 1;
-        column = 2;
-    }
-    else if (digit == 7) {
-        row = 2;
-        column = 0;
-    }
-    else if (digit == 8) {
-        row = 2;
-        column = 1;
-    }
-    else if (digit == 9) {
-        row = 2;
-        column = 2;
     }
     else {
         cout << "Invalid" << endl;
         return;
     }
     
-    if (space[row][column] != 'x' && space[row][column] != '0') {
-        if (token == 'x') {
-            space[row][column] = 'x';
-            token = '0';
+    if (digit >= 1 && digit <= 9) {
+        digit -= 1;
+        row = digit / 3;
+        column = digit % 3;
+        
+        if (space[row][column] != 'x' && space[row][column] != '0') {
+            if (token == 'x') {
+                space[row][column] = 'x';
+                token = '0';
+            }
+            else if (token == '0') {
+                space[row][column] = '0';
+                token = 'x';
+            }
         }
-        else if (token == '0') {
-            space[row][column] = '0';
-            token = 'x';
+        else {
+            cout << "There is no empty space" << endl;
+            function2();
         }
     }
     else {
-        cout << "There is no empty space" << endl;
+        cout << "Invalid" << endl;
         function2();
     }
 }
@@ -98,15 +72,24 @@ bool functionthree() {
         return true;
     }
     
+    bool isBoardFull = true;
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
             if (space[i][j] != 'x' && space[i][j] != '0') {
-                return false;
+                isBoardFull = false;
+                break;
             }
+        }
+        if (!isBoardFull) {
+            break;
         }
     }
     
-    tie = true;
+    if (isBoardFull) {
+        tie = true;
+        return true;
+    }
+    
     return false;
 }
 
